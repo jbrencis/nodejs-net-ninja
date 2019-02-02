c7585e86f2b5bd9c9d0f7e8bd3d3c84b2123020e
Jevgenijs Brencis <jevgenijs.brencis@gmail.com>
2019-02-02 21:30:58 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+//************************** setTimeout**************************
+// ***************************************************************/
+
+// const num = 20;
+// setTimeout(num => console.log(`${num} seconds are passed`), 2000, [num]);
+// 20 seconds are passed
+
+//************************** setInterval**************************
+// ***************************************************************/
+
+// let time = 2;
+// const timer = setInterval(() => {
+//   time += 2;
+//   console.log(`${time} seconds have passed`);
+
+//   if (time > 5) {
+//     clearInterval(timer);
+//   }
+// }, 2000);
+
+/************************** dirname************************** 
+// ***************************************************************/
+
+// console.log(__dirname); ///Users/jevgenijsbrencis/Desktop/Udemy/React_Udemy/Net-Ninja-Youtube-projects/my-node-js-playlist/nodejs-net-ninja
+
+/************************** __filename************************** 
+// ***************************************************************/
+
+console.log(__filename);
+// /Users/jevgenijsbrencis/Desktop/Udemy/React_Udemy/Net-Ninja-Youtube-projects/my-node-js-playlist/nodejs-net-ninja/app.js
