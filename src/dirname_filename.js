955ea9efa149a7a9e8be1646397913b454ecef01
Jevgenijs Brencis <jevgenijs.brencis@gmail.com>
2019-02-03 11:39:07 +0200

    some new additions

---
+++ src/dirname_filename.js
@@ -0,0 +1,11 @@
+/************************** dirname ************************** 
+// ***************************************************************/
+
+console.log(__dirname); ///Users/jevgenijsbrencis/Desktop/Udemy/React_Udemy/Net-Ninja-Youtube-projects/my-node-js-playlist/nodejs-net-ninja
+
+/************************** __filename ************************** 
+// ***************************************************************/
+
+console.log(__filename);
+
+// /Users/jevgenijsbrencis/Desktop/Udemy/React_Udemy/Net-Ninja-Youtube-projects/my-node-js-playlist/nodejs-net-ninja/app.js
